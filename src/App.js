56e26f42c2bc2fea9af17e7a03f15834dc3c24dc
Grace Lee <gracelee9901@gmail.com>
2023-02-04 16:28:53 -0800

    use hooks to track and set state as user types into textarea

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
-    return (
-        <div>
-            <h1 className='title'>Speed Typing Game</h1>
-            <textarea />
-            <h1>Time Remaining: 10</h1>
-            <button>Start</button>
-            <h1>Word Count: 11</h1>
-        </div>
-    )
+  const [typedWords, setTypedWords] = useState("");
+
+  function handleChange(e) {
+    const {value} = e.target;
+    setTypedWords(value);
+  }
+
+  return (
+    <div>
+      <h1 className='title'>Speed Typing Game</h1>
+      <textarea value={typedWords} onChange={handleChange}/>
+      <h1>Time Remaining: 10</h1>
+      <button>Start</button>
+      <h1>Word Count: 11</h1>
+    </div>
+  )
 }
 
 export default App;
