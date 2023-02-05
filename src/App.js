9d22900f227418a2dd70e2590bfe90a653f0d75c
Grace Lee <gracelee9901@gmail.com>
2023-02-04 16:58:42 -0800

    Add word count function and state to hold current value of the timer

--- src/App.js
+++ src/App.js
@@ -3,17 +3,23 @@ import React, { useState } from 'react';
 
 function App() {
   const [typedWords, setTypedWords] = useState("");
+  const [time, setTime] = useState(20);
 
   function handleChange(e) {
     const {value} = e.target;
     setTypedWords(value);
   }
 
+  function calculateWordCount(text) {
+    const wordsArr = text.trim().split(" ")
+    return wordsArr.filter(word => word !== "").length
+  }
+
   return (
     <div>
       <h1 className='title'>Speed Typing Game</h1>
       <textarea value={typedWords} onChange={handleChange}/>
-      <h1>Time Remaining: 10</h1>
+      <h1>Time Remaining: {time}</h1>
       <button>Start</button>
       <h1>Word Count: 11</h1>
     </div>
