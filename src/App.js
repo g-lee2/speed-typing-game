ac5bec6b936465bf75283e3d72a8d26d4d21e1a8
Grace Lee <gracelee9901@gmail.com>
2023-02-06 17:34:20 -0800

    Create new state and update word count when timer turns 0

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   const [typedWords, setTypedWords] = useState("");
   const [time, setTime] = useState(10);
   const [runTimer, setRunTimer] = useState(false);
+  const [wordCount, setWordCount] = useState(0);
 
   function handleChange(e) {
     const {value} = e.target;
@@ -12,17 +13,20 @@ function App() {
   }
 
   function calculateWordCount(text) {
-    const wordsArr = text.trim().split(" ")
-    return wordsArr.filter(word => word !== "").length
+    const wordsArr = text.trim().split(" ");
+    return wordsArr.filter(word => word !== "").length;
   }
-  
+
   useEffect(() => {
       if(runTimer && time > 0) {
           setTimeout(() => {
               setTime(times => times - 1)
-          }, 1000)
+          }, 1000);
       } else if (time === 0) {
-        setRunTimer(false)
+        setRunTimer(false);
+        const totalWordCount = calculateWordCount(typedWords);
+        setWordCount(totalWordCount);
+
       }
   }, [runTimer, time])
 
@@ -32,7 +36,7 @@ function App() {
       <textarea value={typedWords} onChange={handleChange}/>
       <h1>Time Remaining: {time}</h1>
       <button onClick={() => setRunTimer(true)}>Start</button>
-      <h1>Word Count: 11</h1>
+      <h1>Word Count: {wordCount}</h1>
     </div>
   )
 }
